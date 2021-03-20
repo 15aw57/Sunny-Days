425e33142afd8ec87d41069ad5d77f4da2a7da71
davegusta <yifanduan64@gmail.com>
2021-03-19 21:56:43 -0400

    Summary of the data visualized

--- data_summary.R
+++ data_summary.R
@@ -3,7 +3,7 @@ library(dplyr)
 library(ggmap)
 library(mapproj)
 
-#host, country (DONE), collection date (DONE), genome length
+#host, country (DONE), release year (DONE), genome length (DONE)
 dat <- read.csv("./input/info.csv")
 
 dat$length <- as.factor(dat$length)
@@ -23,14 +23,26 @@ ggplot(collection_map, aes(long, lat, group = group))+
   geom_polygon(aes(fill = Freq), color = "white") +
   scale_fill_viridis_c(option = "C")
 
-ggplot(country_df, aes(x = region, y = Freq)) + geom_col()
-
+# release year
 ggplot(dat, aes(x = as.numeric(release_year))) + geom_bar() +
-  scale_x_continuous(breaks = seq(1999, 2015, by = 1))
+  scale_x_continuous(name = "Collection date", breaks = seq(1999, 2015, by = 2))
 
-ggplot(count.data, aes(x = "", y = prop, fill = class)) +
-  geom_bar(width = 1, stat = "identity", color = "white") +
-  coord_polar("y", start = 0)+
-  geom_text(aes(y = lab.ypos, label = prop), color = "white")+
-  scale_fill_manual(values = mycols) +
-  theme_void()
+# genome length
+ggplot(dat, aes(x = "", y = length, fill = length)) + 
+  geom_bar(width = 1, stat = "identity") + 
+  coord_polar("y", start = 0) + theme_void() + ggtitle("Genome Length")
+
+# host
+host_count <- count(dat, host)
+common_name <- c("Unknown", "Mosquito (Aedes)", "Bird", "Crow", "Mosquito (Culex)", 
+                 "Mosquito (Cuclicidae)", "Mosquito (Culiseta)", "Ticks (Dermacentor)", "Horse",
+                 "Human", "Ticks (Hyalomma)", "Oriolus (Bird)", "Ticks (Ornithodoros)", "Fruit bat",
+                 "Tissue culture")
+species <- c("Unknown", "Mosquito", "Bird", "Bird", "Mosquito", "Mosquito", "Mosquito", "Ticks",
+             "Horse", "Human", "Ticks", "Bird", "Ticks", "Bat", "Tissue")
+
+host_count <- cbind(host_count, common_name, Species = as.factor(species))
+
+host_count %>% ggplot(aes(x = reorder(common_name, n), y = n, fill = Species)) + 
+  geom_col() + coord_flip() + 
+  scale_x_discrete(name = "Species") + scale_y_continuous(name = "Count")
--- pim.R
+++ pim.R
@@ -1,6 +1,4 @@
-setwd("C:/Users/yifan/Desktop/winter2021/BIOL432/432FinalProject")
-
-data_CDS <- read.table('pim.txt', header = F, sep = "")
+data_CDS <- read.table('./input/pim.txt', header = F, sep = "")
 rnames <- data_CDS[,1]
 data_CDS2 <- data.matrix(data_CDS[, 2:ncol(data_CDS)])
 rownames(data_CDS2) <- rnames
@@ -14,7 +12,7 @@ my_palette = colorRampPalette(c("white", "blue"))(n = 11)
 require(gplots)
 
 #Prepare heatmaps
-my_palette = colorRampPalette(c("white","blue"))(n = 10)
+my_palette = colorRampPalette(c("white", "dark blue"))(n = 5)
 heatmap.2(data_CDS2, trace = c("none"), density.info = c("none"), 
-          breaks = c(90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100), col = my_palette, 
+          breaks = c(90, 92, 94, 96, 98, 100), col = my_palette, 
           lmat = matrix(c(4,2,3,1), nrow = 2, ncol = 2), key = T, labRow = rnames, labCol = rnames)
